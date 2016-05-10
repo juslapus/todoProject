0c5e846d64430e20cec32c27fc3d5fbc27182358
Justin Lapus <juslapus@gmail.com>
2016-05-10 19:46:36 +0900

    Added the todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,26 @@
 class TodoItemsController < ApplicationController
+	before_action :set_todo_list, :set_todo_item
+
+	def create
+		@todo_item = @todo_list.todo_items.create(item_params)
+
+		redirect_to @todo_list
+	end
+
+	def show
+		@todo_item = TodoItem.find(params[:id])
+	end
+
+	private
+	    def set_todo_list
+	      @todo_list = TodoList.find(params[:todo_list_id])
+	    end
+
+		def item_params
+			params.require(:todo_item).permit(:item)
+		end
+
+		def set_todo_item
+			@todo_item = TodoItem.find(params[:id])
+		end
 end
